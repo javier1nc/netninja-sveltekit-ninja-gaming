c21202960afb60e0e4c1e0b80fa8e31fcddb685b
Javier Sánchez Mendoza <code@javier1nc.com>
2023-03-11 20:06:05 +0000

    sync

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+ let title = 'Welcome'
+
+ const uptateTitle = () => {
+    title = "something completely diferent"
+ }
+
+ 
+</script>
+
+<div class="index">
+    <h2>{title}</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi provident labore perspiciatis voluptatem possimus similique dolores repudiandae blanditiis atque ut temporibus quod fugiat odit, amet magnam facere, doloremque omnis quas.</p>
+
+    <button on:click={uptateTitle}>change title</button>
+    <input type="text" bind:value={title}>
+
+    <div>
+        <a href="/guides">View Guides</a>
+        <a href="/about">About</a>
+    </div>
+
+    
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero ducimus hic quidem dignissimos nam aperiam blanditiis tempore. Aperiam nulla ullam libero labore reprehenderit earum atque. Consequuntur delectus et ut cumque?</p>
+    <div>
+        <a href="/guides">View Guides</a>
+        <a href="/">Home</a>
+    </div>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
